be7f5fb1a4a53fc556d9973eaa4a205d87f1c1a3
Aris Ripandi <riipandi@gmail.com>
2022-09-01 18:04:43 +0700

    fix tauri config for build

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,14 +3,40 @@
     windows_subsystem = "windows"
 )]
 
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
+
+// fn main() -> wry::Result<()> {
+//     use wry::{
+//         application::{
+//             event::{Event, StartCause, WindowEvent},
+//             event_loop::{ControlFlow, EventLoop},
+//             window::WindowBuilder,
+//         },
+//         webview::WebViewBuilder,
+//     };
+
+//     let event_loop = EventLoop::new();
+//     let window = WindowBuilder::new()
+//         .with_title("Flam Drumming")
+//         .build(&event_loop)?;
+//     let _webview = WebViewBuilder::new(window)?
+//         .with_url("https://web.telegram.org/")?
+//         .build()?;
+
+//     event_loop.run(move |event, _, control_flow| {
+//         *control_flow = ControlFlow::Wait;
+
+//         match event {
+//             Event::NewEvents(StartCause::Init) => println!("Wry has started!"),
+//             Event::WindowEvent {
+//                 event: WindowEvent::CloseRequested,
+//                 ..
+//             } => *control_flow = ControlFlow::Exit,
+//             _ => (),
+//         }
+//     });
+// }
